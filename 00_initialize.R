8c64c99a2395abae8e1b6e3b0186e1535d7af393
trriley <im.triley@gmail.com>
2019-12-19 10:19:22 -0600

    Update 00_initialize.R
    Adds tmap_options to cut down on amount of code in 04_create-graphics.R
    
    Reformats comments and adds clarification
    
    Adds helper function for formatting filenames
    
    Creates directories if they don't already exist to avoid errors

--- 00_initialize.R
+++ 00_initialize.R
@@ -18,12 +18,25 @@ packages <- c(
 
 invisible(lapply(packages, library, character.only = TRUE))
 options(tigris_use_cache = TRUE) # causes geometries to be stored in cache instead of re-loaded every time
+tmap_options(
+  show.messages = FALSE,
+  output.dpi = 300
+  )
 
 # initialize global variables and set environments -------------------------------
 inputDataDirectory <- here::here("data", "input")
-outputDataDirectory <- here::here("data", "output")
-outputChartDirectory <- here::here("chart")
-outputMapDirectory <- here::here("map")
+outputDataDirectory <- here::here("data", "output") %T>%
+  if(!dir.exists(.)) {
+    dir.create(.)
+  }
+outputChartDirectory <- here::here("chart") %T>%
+  if(!dir.exists(.)) {
+    dir.create(.)
+  }
+outputMapDirectory <- here::here("map") %T>%
+  if(!dir.exists(.)) {
+    dir.create(.)
+  }
 censusDataInventoryFile <- paste(
   inputDataDirectory,
   "census-data-inventory.csv",
@@ -33,14 +46,29 @@ censusDataInventory <- readr::read_csv(censusDataInventoryFile)
 censusAPIKey <- "2f44a09c684e6b031d3e76f3655c169c167aeba8"
 Sys.setenv(CENSUS_KEY = censusAPIKey)
 acs2017VariableTable <- tidycensus::load_variables(2017, "acs5", cache = TRUE)
+acs2018VariableTable <- tidycensus::load_variables(2018, "acs5", cache = TRUE)
+region6CountyList <- c("113", "053", "105", "075", "091")
+acsYear <- 2018                                              # !!CHANGE THIS WHEN UPDATED DATA IS RELEASED!!
 
-# initialize global functions ---------------------------------------------
+
+# define global functions -------------------------------------------------
 getArea <- function(sf) {
+  # create new column with calculated area in square miles
   dplyr::mutate(sf, area_sq_mi = units::set_units(sf::st_area(geometry), mi^2))
-} # function to return column with area of geographies
+}
 coalesceByColumn <- function(df) {
+  # coalesce a table by columns -- https://stackoverflow.com/a/45515491
+  # this is used to clean the ACS data up after it is retrieved from the
+  # API
   return(dplyr::coalesce(!!! as.list(df)))
-} # useful function for coalescing a table by columns -- https://stackoverflow.com/a/45515491
+}
+addACSYearsToFilename <- function(filename, acsYear) {
+  require(stringr)
+  firstPart <- str_split(filename, "[.]", simplify = TRUE)[1]
+  secondPart <- paste("_ACS-", (acsYear - 4), "-", acsYear, ".", sep = "")
+  thirdPart <- str_split(filename, "[.]", simplify = TRUE)[2]
+  paste0(firstPart, secondPart, thirdPart, sep = "")
+}
 
 # source other scripts to begin processing --------------------------------
 source(here::here("01_retrieve-data.R"))
