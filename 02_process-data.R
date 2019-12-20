a4bd6577a9b563dd29b2d4b85423bd4f2cd4ffcb
trriley <im.triley@gmail.com>
2019-12-20 11:51:48 -0600

    Update 02_process-data.R
    Adds denominator variables

--- 02_process-data.R
+++ 02_process-data.R
@@ -1,20 +1,20 @@
 # create tibble to store relationship between desired variable's name,
 # its description, and the ACS variables from which it is derived
 acsVariableTibble <- tibble::tribble(
-  ~short_name, ~long_name, ~acs_variables_2017,
-  "est_pop", "Population Estimate", est_pop <- c("B01001_001E"),
-  "est_blw18", "Population Under 18 Estimate", est_blw18 <- paste0("B01001_0", stringr::str_pad(c(3:6,27:30), 2, "left", pad = "0"), "E"),
-  "est_18to65", "Population Between 18 and 65 Estimate", est_18to65 <- paste0("B01001_0", stringr::str_pad(c(7:9,10:19,31:43), 2, "left", pad = "0"), "E"),
-  "est_ovr65", "est_ovr65", est_ovr65 <- paste0("B01001_0", c(20:25, 44:49), "E"),
-  "est_vet", "est_vet", est_vet <- "B21001_002E",
-  "est_dsblty", "est_dsblty", est_dsblty <- subset(acs2017VariableTable, stringr::str_detect(name, "B18101_") & stringr::str_detect(label, "With a disability")) %>% .$name %>% paste(.,"E",sep=""),
-  "est_blwpov", "est_blwpov", est_blwpov <- "B17020_002E",
-  "est_nocars", "est_nocars", est_nocars <- "B08141_002E",
-  "gini", "GINI Coefficient", gini <- "B19083_001E",
-  "est_nodipl", "est_nodipl", est_nodipl <- paste0("B15003_0", stringr::str_pad(c(2:16), 2, "left", pad = "0"), "E"),
-  "inc_percap", "inc_percap", inc_percap <- "B19301_001E",
-  "inc_medhh", "inc_medhh", inc_medhh <- "B22008_001E",
-  "est_hhSo65", "est_hhSo65", est_hhSo65 <- "B22001_003E"
+  ~short_name, ~long_name, ~acs_variables_2017, ~denominator,
+  "est_pop", "Population Estimate", est_pop <- c("B01001_001E"), NULL,
+  "est_blw18", "Population Under 18 Estimate", est_blw18 <- paste0("B01001_0", stringr::str_pad(c(3:6,27:30), 2, "left", pad = "0"), "E"), dnm_blw18 <- "B01001_001E",
+  "est_18to65", "Population Between 18 and 65 Estimate", est_18to65 <- paste0("B01001_0", stringr::str_pad(c(7:9,10:19,31:43), 2, "left", pad = "0"), "E"), dnm_18to65 <- "B01001_001E",
+  "est_ovr65", "est_ovr65", est_ovr65 <- paste0("B01001_0", c(20:25, 44:49), "E"), dnm_ovr65 <- "B01001_001E",
+  "est_vet", "est_vet", est_vet <- "B21001_002E", dnm_vet <- "B21001_001E",
+  "est_dsblty", "est_dsblty", est_dsblty <- subset(acs2017VariableTable, stringr::str_detect(name, "B18101_") & stringr::str_detect(label, "With a disability")) %>% .$name %>% paste(.,"E",sep=""), dnm_dsblty <- "B18101_001E",
+  "est_blwpov", "est_blwpov", est_blwpov <- "B17020_002E", dnm_blwpov <- "B17020_001E",
+  "est_nocars", "est_nocars", est_nocars <- "B08141_002E", dnm_nocars <- "B08141_001E",
+  "gini", "GINI Coefficient", gini <- "B19083_001E", NULL,
+  "est_nodipl", "est_nodipl", est_nodipl <- paste0("B15003_0", stringr::str_pad(c(2:16), 2, "left", pad = "0"), "E"), dnm_nodipl <- "B15003_001E",
+  "inc_percap", "inc_percap", inc_percap <- "B19301_001E", NULL,
+  "inc_medhh", "inc_medhh", inc_medhh <- "B22008_001E", NULL,
+  "est_hhSo65", "est_hhSo65", est_hhSo65 <- "B22001_003E", dnm_hhSo65 <- "B22001_001E"
 )
 
 # create function to print warnings when a tibble has already been transmuted
@@ -47,7 +47,16 @@ tryCatch(
         est_nodipl = rowSums(.[names(.) %in% est_nodipl]),
         inc_percap = rowSums(.[names(.) %in% inc_percap]),
         inc_medhh = rowSums(.[names(.) %in% inc_medhh]),
-        est_hhSo65 = rowSums(.[names(.) %in% est_hhSo65])
+        est_hhSo65 = rowSums(.[names(.) %in% est_hhSo65]),
+        dnm_blw18 = rowSums(.[names(.) %in% dnm_blw18]),
+        dnm_18to65 = rowSums(.[names(.) %in% dnm_18to65]),
+        dnm_ovr65 = rowSums(.[names(.) %in% dnm_ovr65]),
+        dnm_vet = rowSums(.[names(.) %in% dnm_vet]),
+        dnm_dsblty = rowSums(.[names(.) %in% dnm_dsblty]),
+        dnm_blwpov = rowSums(.[names(.) %in% dnm_blwpov]),
+        dnm_nocars = rowSums(.[names(.) %in% dnm_nocars]),
+        dnm_nodipl = rowSums(.[names(.) %in% dnm_nodipl]),
+        dnm_hhSo65 = rowSums(.[names(.) %in% dnm_hhSo65])
       )
   },
   error = function(err) {
@@ -75,7 +84,16 @@ tryCatch(
         est_nodipl = rowSums(.[names(.) %in% est_nodipl]),
         inc_percap = rowSums(.[names(.) %in% inc_percap]),
         inc_medhh = rowSums(.[names(.) %in% inc_medhh]),
-        est_hhSo65 = rowSums(.[names(.) %in% est_hhSo65])
+        est_hhSo65 = rowSums(.[names(.) %in% est_hhSo65]),
+        dnm_blw18 = rowSums(.[names(.) %in% dnm_blw18]),
+        dnm_18to65 = rowSums(.[names(.) %in% dnm_18to65]),
+        dnm_ovr65 = rowSums(.[names(.) %in% dnm_ovr65]),
+        dnm_vet = rowSums(.[names(.) %in% dnm_vet]),
+        dnm_dsblty = rowSums(.[names(.) %in% dnm_dsblty]),
+        dnm_blwpov = rowSums(.[names(.) %in% dnm_blwpov]),
+        dnm_nocars = rowSums(.[names(.) %in% dnm_nocars]),
+        dnm_nodipl = rowSums(.[names(.) %in% dnm_nodipl]),
+        dnm_hhSo65 = rowSums(.[names(.) %in% dnm_hhSo65])
       )
   },
   error = function(err) {
@@ -102,7 +120,16 @@ tryCatch(
         # est_nodipl = rowSums(.[names(.) %in% est_nodipl]),
         inc_percap = rowSums(.[names(.) %in% inc_percap]),
         # inc_medhh = rowSums(.[names(.) %in% inc_medhh]),
-        # est_hhSo65 = rowSums(.[names(.) %in% est_hhSo65])
+        # est_hhSo65 = rowSums(.[names(.) %in% est_hhSo65]),
+        dnm_blw18 = rowSums(.[names(.) %in% dnm_blw18]),
+        dnm_18to65 = rowSums(.[names(.) %in% dnm_18to65]),
+        dnm_ovr65 = rowSums(.[names(.) %in% dnm_ovr65]),
+        dnm_vet = rowSums(.[names(.) %in% dnm_vet])
+        # dnm_dsblty = rowSums(.[names(.) %in% dnm_dsblty]),
+        # dnm_blwpov = rowSums(.[names(.) %in% dnm_blwpov]),
+        # dnm_nocars = rowSums(.[names(.) %in% dnm_nocars]),
+        # dnm_nodipl = rowSums(.[names(.) %in% dnm_nodipl]),
+        # dnm_hhSo65 = rowSums(.[names(.) %in% dnm_hhSo65])
       )
   },
   error = function(err) {
